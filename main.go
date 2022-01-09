af151268cf772cc2e21f3ad69b889da413e32977
Jan Ritter <git@janrtr.de>
2022-01-09 23:35:59 +0100

    feat: added main with jaeger tracing provider and Webserver start

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"log"
+
+	"github.com/janritter/kvb-api/adapters"
+	"github.com/janritter/kvb-api/services"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/jaeger"
+	"go.opentelemetry.io/otel/sdk/resource"
+	tracesdk "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+)
+
+const (
+	service = "kvb-api"
+)
+
+func tracerProvider(url string) (*tracesdk.TracerProvider, error) {
+	// Create the Jaeger exporter
+	exp, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(url)))
+	if err != nil {
+		return nil, err
+	}
+	tp := tracesdk.NewTracerProvider(
+		// Always be sure to batch in production.
+		tracesdk.WithBatcher(exp),
+		// Record information about this application in an Resource.
+		tracesdk.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceNameKey.String(service),
+		)),
+	)
+	return tp, nil
+}
+
+func main() {
+	tp, err := tracerProvider("http://localhost:14268/api/traces")
+	if err != nil {
+		log.Fatal(err)
+	}
+	otel.SetTracerProvider(tp)
+
+	KVBAdapter := adapters.NewKVBAdapter()
+	StationMapperAdapter := adapters.NewStationMapperAdapter()
+	DepartureService := services.New(StationMapperAdapter, KVBAdapter)
+	WebServerAdapter := adapters.NewWebserverAdapter(DepartureService)
+
+	WebServerAdapter.RunWebserver()
+}
