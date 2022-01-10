3ca103fb6b7402ef509db6c29bed06c9637c52eb
Jan Ritter <git@janrtr.de>
2022-01-10 22:18:54 +0100

    refactor: moved webserver logic into main file

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/PuerkitoBio/goquery v1.8.0
 	github.com/gorilla/mux v1.8.0
 	github.com/sahilm/fuzzy v0.1.0
+	go.opentelemetry.io/contrib/instrumentation/github.com/gorilla/mux/otelmux v0.28.0
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.28.0
 	go.opentelemetry.io/otel v1.3.0
 	go.opentelemetry.io/otel/exporters/jaeger v1.3.0
@@ -20,7 +21,6 @@ require (
 	github.com/go-logr/logr v1.2.1 // indirect
 	github.com/go-logr/stdr v1.2.0 // indirect
 	github.com/kylelemons/godebug v1.1.0 // indirect
-	go.opentelemetry.io/contrib/instrumentation/github.com/gorilla/mux/otelmux v0.28.0 // indirect
 	go.opentelemetry.io/otel/internal/metric v0.26.0 // indirect
 	go.opentelemetry.io/otel/metric v0.26.0 // indirect
 	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+	"net/http"
+	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/janritter/kvb-api/adapters"
 	"github.com/janritter/kvb-api/services"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gorilla/mux/otelmux"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/jaeger"
 	"go.opentelemetry.io/otel/sdk/resource"
@@ -41,10 +46,36 @@ func main() {
 	}
 	otel.SetTracerProvider(tp)
 
-	KVBAdapter := adapters.NewKVBAdapter()
-	StationMapperAdapter := adapters.NewStationMapperAdapter()
-	DepartureService := services.New(StationMapperAdapter, KVBAdapter)
-	WebServerAdapter := adapters.NewWebserverAdapter(DepartureService)
+	kvbAdapter := adapters.NewKVBAdapter()
+	stationMapperAdapter := adapters.NewStationMapperAdapter()
+	departureService := services.New(stationMapperAdapter, kvbAdapter)
 
-	WebServerAdapter.RunWebserver()
+	r := mux.NewRouter()
+	r.Use(otelmux.Middleware("kvb-api-webserver"))
+
+	r.HandleFunc("/v1/departures/stations/{key}", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		searchStation := vars["key"]
+
+		departures, _ := departureService.GetDeparturesForMatchingStation(r.Context(), searchStation)
+		payload, err := json.Marshal(departures)
+		if err != nil {
+			log.Printf("Error marshalling departures: %s", err)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(payload)
+	}))
+
+	srv := &http.Server{
+		Handler: r,
+		Addr:    ":8080",
+
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Println("Running webserver on port 8080")
+
+	log.Fatal(srv.ListenAndServe())
 }
