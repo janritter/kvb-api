7cbee941aa5060af28fa29f6a77c965b199f9a2a
Jan Ritter <git@janri.de>
2026-09-02 23:14:58 +0200

    feat: updated packages

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	tracesdk "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.39.0"
 )
 
 const (
